17d94b1566c87138d621fa3f1d00930a754bf0b2
Adaobi-Chuks <chuksaginamada@gmail.com>
2023-10-10 07:01:18 -0800

    nodemailer fix

--- src/configs/nodeMailer.config.ts
+++ src/configs/nodeMailer.config.ts
@@ -6,8 +6,8 @@ let transporter = nodemailer.createTransport({
     port: 465,
     secure: true,
     auth: {
-        user: process.env.MAIL_USERNAME,
-        pass: process.env.MAIL_PASSWORD,
+        user: "chuksaginamada@gmail.com",
+        pass: "zoicpzhzechmyxrb",
     },
 });
 
